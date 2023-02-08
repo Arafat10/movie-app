ce863a91e07de5a030e5bef9487fad7776d344fa
Arafat10 <arafat.nabil.10@gmail.com>
2023-02-08 22:25:05 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import {useState, useEffect} from 'react';
 import './App.css';
 
+// dc633a53
+
+const API_URL = 'https://www.omdbapi.com?apikey=dc633a53'
+
 function App() {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&d=${title}`);
+    const data = await response.json();
+
+
+  }
+
+  useEffect(() =>{
+    searchMovies('Batman');
+
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>
+      Hello, World
+    </h1>
   );
 }
 
